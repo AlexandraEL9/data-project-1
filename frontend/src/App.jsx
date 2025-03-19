1e42f4d9080bbdcf6929c42ea68d8ed15955a6ce
AlexandraEL9 <alexandraelee9@gmail.com>
2025-03-19 16:49:31 +0000

    css for navtabs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,23 @@
-import { useState } from "react";
-import SalesForm from "./components/SalesForm";
-import SalesTable from "./components/SalesTable";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import NavTabs from "./components/NavTabs";
+import Sandwiches from "./pages/Sandwiches";
+import Drinks from "./pages/Drinks";
+import Snacks from "./pages/Snacks";
 
 function App() {
-  const [refresh, setRefresh] = useState(false);
-
   return (
-    <div>
-      <h1>Sandwich Sales Tracker</h1>
-      <SalesForm onSaleAdded={() => setRefresh(!refresh)} />
-      <SalesTable refresh={refresh} />
-    </div>
+    <Router>
+      <div>
+        <h1>Sandwich Sales POS</h1>
+        <NavTabs />
+        <Routes>
+          <Route path="/" element={<Navigate to="/sandwiches" />} />
+          <Route path="/sandwiches" element={<Sandwiches />} />
+          <Route path="/drinks" element={<Drinks />} />
+          <Route path="/snacks" element={<Snacks />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
